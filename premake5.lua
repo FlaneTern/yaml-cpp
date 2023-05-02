77cfe4144cdf8df92963318f5cc7a2f92a9ad2bb
luciennezzz <hertgeathertmc@gmail.com>
2023-05-02 20:08:22 +0700

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-itm/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/yaml-cpp/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines "YAML_CPP_STATIC_DEFINE"
+
+	systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
